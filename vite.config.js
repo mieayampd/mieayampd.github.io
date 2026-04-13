f46598fe0367e4ff9a3db74ed076f12407793dde
mieayampd <mie.ayam.rich@gmail.com>
2026-04-13 21:08:59 +0700

    chore: add automated image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,57 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false, // keep viewBox because it's important for scaling
+                noSpaceAfterFlags:  true,
+              },
+            },
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        lossless: false,
+        quality: 80,
+      },
+      avif: {
+        lossless: false,
+        quality: 70,
+      },
+    }),
   ],
   server: {
     port: 3000,
